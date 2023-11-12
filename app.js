c81445e4c8f31b188e12411fd53960185b5959cf
Steven Ngo <stevenxngo@login-students.khoury.northeastern.edu>
2023-11-12 03:54:17 -0500

    lab5

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-// const express = require("express");
 import express from "express";
-import HelloRoutes from "./hello.js";
-import Lab5 from "./lab5.js";
-import CourseRoutes from "./courses/routes.js";
 import cors from "cors";
+import Hello from "./hello.js";
+import Lab5 from "./lab5.js";
 
 const app = express();
-
 app.use(cors());
-
 app.use(express.json());
-
-CourseRoutes(app);
+Hello(app);
 Lab5(app);
-HelloRoutes(app);
-
 app.listen(4000);
