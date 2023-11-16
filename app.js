d7507446c00f4c3b0db9bb28d8694c1a17e7b0f2
Steven Ngo <stevenxngo@login-students.khoury.northeastern.edu>
2023-11-16 16:08:11 -0500

    finish a5

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ import express from "express";
 import cors from "cors";
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import AssignmentRoutes from "./assignments/routes.js";
+import "dotenv/config";
 
 const app = express();
-app.use(cors());
 app.use(express.json());
-Hello(app);
+app.use(
+    cors({
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    })
+);  
 Lab5(app);
-app.listen(4000);
+Hello(app);
+CourseRoutes(app);
+ModuleRoutes(app);
+AssignmentRoutes(app);
+app.listen(process.env.PORT || 4000);
--- courses/routes.js
+++ courses/routes.js
@@ -1,40 +1,65 @@
 import Database from "../Database/index.js";
 function CourseRoutes(app) {
   app.get("/api/courses", (req, res) => {
+    // console.log("GET /api/courses")
     const courses = Database.courses;
     res.send(courses);
   });
-
-  app.delete("/api/courses/:id", (req, res) => {
+  app.get("/api/courses/:id", (req, res) => {
+    // console.log("GET /api/courses/:id")
     const { id } = req.params;
     const course = Database.courses.find((c) => c._id === id);
     if (!course) {
-      res.sendStatus(404);
+      res.status(404).send("Course not found");
       return;
     }
+    res.send(course);
+  });
+  app.get("/api/courses/:id/title", (req, res) => {
+    // console.log("GET /api/courses/:id/title")
+    const { id } = req.params;
+    const course = Database.courses.find((c) => c._id === id);
+    if (!course) {
+      res.status(404).send("Course not found");
+      return;
+    }
+    const { number, startDate } = course;
+    const [startYear, startDay] = startDate.split("-");
+    res.send(`${number}.${startYear}${startDay}`);
+  });
+  app.get("/api/courses/:id/description", (req, res) => {
+    // console.log("GET /api/courses/:id/description")
+    const { id } = req.params;
+    const course = Database.courses.find((c) => c._id === id);
+    if (!course) {
+      res.status(404).send("Course not found");
+      return;
+    }
+    const { startDate } = course;
+    const [startYear, startMonth, startDay] = startDate.split("-");
+    res.send(
+      `${startYear}${startMonth}_${startDay} Fall ${startYear} Semester Full Term`
+    );
+  });
+  app.put("/api/courses/:id", (req, res) => {
+    // console.log("PUT /api/courses/:id")
+    const { id } = req.params;
+    const course = req.body;
+    Database.courses = Database.courses.map((c) =>
+      c._id === id ? { ...course, c } : c
+    );
+    res.send(course);
+  });
+  app.delete("/api/courses/:id", (req, res) => {
+    const { id } = req.params;
     Database.courses = Database.courses.filter((c) => c._id !== id);
     res.sendStatus(204);
   });
-
   app.post("/api/courses", (req, res) => {
+    // console.log("POST /api/courses")
     const course = { ...req.body, _id: new Date().getTime().toString() };
     Database.courses.push(course);
     res.send(course);
-  });
-  app.put("/api/courses/:id", (req, res) => {
-    const { id } = req.params;
-    const course = req.body;
-    const currentCourse = Database.courses.find((c) => c._id === id);
-
-    if (!currentCourse) {
-      res.sendStatus(404);
-      return;
-    }
-
-    Database.courses = Database.courses.map((c) =>
-      c._id === id ? { c, ...course } : c
-    );
-    res.sendStatus(204);
   });
 }
 export default CourseRoutes;
