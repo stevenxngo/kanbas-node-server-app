a028599381f5f25f0adb0388994b13e9ad7571eb
Steven Ngo <stevenxngo@login-students.khoury.northeastern.edu>
2023-11-16 18:28:23 -0500

    try to fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
-import cors from "cors";
 import Hello from "./hello.js";
-import Lab5 from "./lab5.js";
+import Lab5 from "./Lab5.js";
+import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignments/routes.js";
@@ -10,11 +10,11 @@ import "dotenv/config";
 const app = express();
 app.use(express.json());
 app.use(
-    cors({
-      credentials: true,
-      origin: process.env.FRONTEND_URL
-    })
-);  
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
 Lab5(app);
 Hello(app);
 CourseRoutes(app);
