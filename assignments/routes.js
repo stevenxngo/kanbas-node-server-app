6cfdb196bb60582105321821708703e9880ae7bc
Steven Ngo <stevenxngo@login-students.khoury.northeastern.edu>
2023-11-19 06:49:44 -0500

    extra credit

--- assignments/routes.js
+++ assignments/routes.js
@@ -18,15 +18,15 @@ function AssignmentRoutes(app) {
     db.assignments.push(newAssignment);
     res.send(newAssignment);
   });
-  app.delete("/api/assignments/:aid", (req, res) => {
-    const { aid } = req.params;
-    db.assignments = db.assignments.filter((a) => a._id !== aid);
-    res.sendStatus(200);
-  });
   app.get("/api/courses/:cid/assignments", (req, res) => {
     const { cid } = req.params;
     const assignments = db.assignments.filter((a) => a.course === cid);
     res.send(assignments);
+  });
+  app.delete("/api/assignments/:aid", (req, res) => {
+    const { aid } = req.params;
+    db.assignments = db.assignments.filter((a) => a._id !== aid);
+    res.sendStatus(200);
   });
 }
 export default AssignmentRoutes;
