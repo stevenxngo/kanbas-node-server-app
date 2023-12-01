9adacf196a1f500d881ad1efb9e714a8836bf003
Steven Ngo <stevenxngo@login-students.khoury.northeastern.edu>
2023-11-30 21:58:54 -0500

    change mongo connection string

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ import session from "express-session";
 import "dotenv/config";
 import mongoose from "mongoose";
 
-const CONNECTION_STRING =
-  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
-mongoose.connect(CONNECTION_STRING);
+process.env.DB_CONNECTION_STRING
+  ? mongoose.connect(process.env.DB_CONNECTION_STRING, { dbName: "kanbas" })
+  : mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
 const app = express();
 app.use(
